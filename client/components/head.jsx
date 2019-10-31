63ac24786fdf38101a011b4fc0d8d23355025dc0
johnwsouth <im.john.south@gmail.com>
2019-10-31 00:58:27 +0000

    added head as red block, added firework onclick function

--- client/components/head.jsx
+++ client/components/head.jsx
@@ -6,14 +6,31 @@ export default class Head extends React.Component {
     super(props);
 
     this.state = {
-      headState: null
+      headState: 'initial'
     };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({ headState: 'isClicked' });
   }
 
   render() {
-    return (
-      <div>I am the Head</div>
-    );
+    if (this.state.headState === 'initial') {
+      return (
+        <>
+        <div className = 'head ' onClick = {this.handleClick}></div>
+        <div className = 'firework-head firework-initial'></div>
+        </>
+      );
+    } else {
+      return (
+        <>
+        <div className = 'head'></div>
+        <div className='firework-head firework-clicked'></div>
+        </>
+      );
+    }
   }
 }
 
