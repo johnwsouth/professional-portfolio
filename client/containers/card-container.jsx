cb115d9c04f2e837eab772aeb28018b21f431e09
johnwsouth <im.john.south@gmail.com>
2019-11-01 20:53:34 +0000

    add animation to block

--- client/containers/card-container.jsx
+++ client/containers/card-container.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import Card from '../components/card';
 
 export default function CardContainer(props) {
   return (
-    <div className='card-container'></div>
+    <div className='card-container'>
+      <Card />
+    </div>
   );
 }
