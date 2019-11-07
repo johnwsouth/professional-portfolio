64fd2c5251b49f5ceb511271296b1b31b0a7df9f
johnwsouth <im.john.south@gmail.com>
2019-11-07 09:14:29 +0000

    added head animation progression as well as ins inline style attribute for head

--- client/components/head.jsx
+++ client/components/head.jsx
@@ -16,11 +16,18 @@ export default class Head extends React.Component {
     this.setState({ headClicked: !this.state.headClicked });
   }
 
+  cycleImages() {
+    for (var index = 1; index < 15; index++) {
+      var headElem = document.querySelector('.head');
+      headElem.attr('background-image', 'url');
+    }
+  }
+
   render() {
     if (this.state.headClicked === false) {
       return (
         <>
-        <div className = 'head' onClick = {this.handleClick}></div>
+          <div className='head' style={{ backgroundImage: "url('./images/head-initial.png')" }} onClick = {this.handleClick}></div>
         <div className = 'head head-bg'></div>
         {/* <div className = 'firework-head firework-initial'></div> */}
         </>
