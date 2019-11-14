2eb722127e803f3beb6c50552cdfb26ed04c213a
johnwsouth <im.john.south@gmail.com>
2019-11-14 01:22:48 +0000

    added fly in animation for about-me component

--- client/components/about-me.jsx
+++ client/components/about-me.jsx
@@ -10,8 +10,12 @@ export default class AboutMe extends React.Component {
 
   render() {
     return (
-      <div className = 'about-me-container'>
+      <div className = 'about-me-container bounce-and-slide-in'>
         <div className = 'about-me-picture'></div>
+        <div className='about-me-text'> &nbsp; &nbsp; My name is John South, and I have always loved to learn new things.
+          Recently I have been exposed to  a profession that has satisfied this hunger. With the knowledge
+          I have gained in JavaScript, extending outwards toward frameworks such as React and NodeJS,
+           I can now build virtually anything. I am excited to continue learning with you.</div>
       </div>
 
     );
