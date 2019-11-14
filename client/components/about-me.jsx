f200a19c4d3bc5ea19914a366fa96663bb32ea0b
johnwsouth <im.john.south@gmail.com>
2019-11-14 07:54:14 +0000

    changed the cards to cirlces

--- client/components/about-me.jsx
+++ client/components/about-me.jsx
@@ -18,7 +18,7 @@ export default class AboutMe extends React.Component {
     if (this.state.aboutMeOn === true) {
       return (
         <div className = 'about-me-container bounce-and-slide-in'>
-          <div className='about-me-title'> About Me </div>
+          <div className='about-me-title hover'> About Me </div>
           <div className = 'about-me-picture hover'></div>
           <div className='about-me-text'> &nbsp; &nbsp; My name is John South, and I have always loved to learn new things.
           Recently I have been exposed to  a profession that has satisfied this hunger. With the knowledge
